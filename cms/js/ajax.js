5ca86276afb9eef6a24746be08cddf09e9f44133
z1019170871 <1019170871@qq.com>
2020-05-15 13:41:21 +0800

    code

--- cms/js/ajax.js
+++ cms/js/ajax.js
@@ -2,11 +2,11 @@
 // 请求方式、请求地址、携带参数、响应内容
 var baseURL = 'http://47.115.38.108:8099';
 function myAjax(url,method,param,successHandle,errHandle){
-  $.ajax({
-    url:baseURL + url,
-    method:method,
-    data:param,
-    success:successHandle,
-    error:errHandle
-  })
+    $.ajax({
+        url:baseURL + url,
+        method:method,
+        data:param,
+        success:successHandle,
+        error:errHandle
+    })
 }
